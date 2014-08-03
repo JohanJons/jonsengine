cfae471689f7c49ea8c4cb0036aae7861259f6ec
jons.johan@gmail.com <jons.johan@gmail.com@3950d6fe-9a8f-9439-057c-e1ce0a5f0c9d>
2014-08-03 13:10:28 +0000

    Added point lights

--- branches/directx11/JonsEngine/include/Renderer/DirectX11/Shaders/PointLightPixel.hlsl
+++ branches/directx11/JonsEngine/include/Renderer/DirectX11/Shaders/PointLightPixel.hlsl
@@ -5,19 +5,36 @@
 
 cbuffer PointLightConstants : register(b0)
 {
-    float4 gAmbientLight;
+    float4x4 gLightWVPMatrix;
+    float4 gLightColor;
+    float4 gLightPosition;
     float2 gScreenSize;
+    float gLightIntensity;
+    float gMaxDistance;
 };
 
-Texture2D gDiffuseTexture : register(t0);
+Texture2D gPositionTexture : register(t0);
+Texture2D gDiffuseTexture : register(t1);
+Texture2D gNormalTexture : register(t2);
 SamplerState gSampler : register(s0);
 
 
 float4 ps_main(float4 position : SV_Position) : SV_Target0
 {
     float2 texcoord = position.xy / gScreenSize;
+    float4 worldPosition = gPositionTexture.Sample(gSampler, texcoord);
+    float4 diffuse = gDiffuseTexture.Sample(gSampler, texcoord);
+    float3 normal = (float3)gNormalTexture.Sample(gSampler, texcoord);
 
-    return float4(1.0, 0.0, 0.0, 1.0);
+    float3 positionDiff = (float3)(gLightPosition - worldPosition);
+    float distance = length(positionDiff);
+    float attenuation = clamp(1.0 - distance * distance * (1 / (gMaxDistance * gMaxDistance)), 0.0, 1.0);
+    attenuation *= attenuation;
+
+    float3 lightDir = normalize(positionDiff);
+    float angleNormal = clamp(dot(normalize(normal), lightDir), 0, 1);
+
+    return diffuse * angleNormal * attenuation * gLightIntensity * gLightColor;
 }
 
 #endif
