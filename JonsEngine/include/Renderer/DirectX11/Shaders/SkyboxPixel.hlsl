a59c3c18318609cd8b289272ada44ccdfda2d823
JohanJons <jons.johan@gmail.com>
2015-06-18 16:57:09 +0200

    work on skybox

--- JonsEngine/include/Renderer/DirectX11/Shaders/SkyboxPixel.hlsl
+++ JonsEngine/include/Renderer/DirectX11/Shaders/SkyboxPixel.hlsl
@@ -2,11 +2,12 @@
 #define SKYBOX_PIXEL_HLSL
 
 Texture2D gSkybox : register(TEXTURE_REGISTER_EXTRA);
+SamplerState gSampler : register(SAMPLER_REGISTER_ANISOTROPIC);
 
 
-float4 ps_main() : SV_Target0
+float4 ps_main(GBufferVSOut input) : SV_Target0
 {
-    return float4(0.0, 1.0, 0.0, 0.0);
+    return gSkybox.Sample(gSampler, input.mWorldPosition.xyz);
 }
 
 #endif
--- JonsEngine/include/Renderer/DirectX11/Shaders/SkyboxVertex.hlsl
+++ JonsEngine/include/Renderer/DirectX11/Shaders/SkyboxVertex.hlsl
@@ -1,9 +1,25 @@
 #ifndef SKYBOX_VERTEX_HLSL
 #define SKYBOX_VERTEX_HLSL
 
-float4 vs_main(uint VertexID: SV_VertexID) : SV_Position
+cbuffer TransformConstants : register(CBUFFER_REGISTER_VERTEX)
 {
-    return float4(float2(((VertexID << 1) & 2) * 2.0f, (VertexID == 0) * -4.0f) + float2(-1.0f, 1.0f), 0.0f, 1.0f);
+    float4x4 gInvVPMatrix;
+};
+
+struct GBufferVSOut
+{
+    float4 mPosition : SV_POSITION;
+    float4 mWorldPosition : WORLDPOS;
+};
+
+
+GBufferVSOut vs_main(uint VertexID: SV_VertexID)
+{
+    GBufferVSOut output;
+    output.mPosition = float4(float2(((VertexID << 1) & 2) * 2.0f, (VertexID == 0) * -4.0f) + float2(-1.0f, 1.0f), 1.0f, 1.0f);
+    output.mWorldPosition = mul(gInvVPMatrix, output.mPosition);
+
+    return output;
 }
 
 #endif
