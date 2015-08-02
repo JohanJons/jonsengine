0e49255f239d514e010f407b13020a09f05a07e9
JohanJons <jons.johan@gmail.com>
2015-08-02 13:24:13 +0200

    Initial commit scene refactoring

--- JonsEngine/include/Resources/ResourceManifest.h
+++ JonsEngine/include/Resources/ResourceManifest.h
@@ -1,49 +1,54 @@
 #pragma once
 
 #include "include/Resources/JonsPackage.h"
+#include "include/Resources/Model.h"
+#include "include/Resources/ModelNode.h"
+#include "include/Resources/Mesh.h"
+#include "include/Resources/Material.h"
+#include "include/Resources/Skybox.h"
 #include "include/Core/Memory/IMemoryAllocator.h"
-#include "include/Core/Containers/DataPool.hpp"
-#include "include/Scene/Model.h"
-#include "include/Scene/ModelNode.h"
-#include "include/Scene/Mesh.h"
-#include "include/Scene/Material.h"
-#include "include/Scene/Skybox.h"
-#include "include/Renderer/DirectX11/DX11Renderer.h"
+#include "include/Core/Containers/IDMap.hpp"
 #include "include/Renderer/Shapes.h"
-
-#include <vector>
-#include <map>
 
 namespace JonsEngine
 {
+    class DX11Renderer;
+
+    typedef IDMap<Model>::ItemID ModelID;
+    typedef IDMap<Model>::ItemID MaterialID;
+    typedef IDMap<Model>::ItemID SkyboxID;
+
     class ResourceManifest
     {
     public:
         ResourceManifest(DX11Renderer& renderer, IMemoryAllocatorPtr memoryAllocator);
         ~ResourceManifest();
 
-        ModelPtr CreateRectangle(const std::string& modelName, const float sizeX, const float sizeY, const float sizeZ, MaterialPtr material);
-        ModelPtr CreateCube(const std::string& modelName, const float size, MaterialPtr material);
-        ModelPtr CreateSphere(const std::string& modelName, const float radius, const uint32_t rings, const uint32_t sectors, MaterialPtr material);
-        ModelPtr LoadModel(const std::string& assetName, const JonsPackagePtr jonsPkg);
-        ModelPtr GetModel(const std::string& modelName);
+        ModelID CreateRectangle(const std::string& modelName, const float sizeX, const float sizeY, const float sizeZ);
+        ModelID CreateCube(const std::string& modelName, const float size);
+        ModelID CreateSphere(const std::string& modelName, const float radius, const uint32_t rings, const uint32_t sectors);
+        ModelID LoadModel(const std::string& assetName, const JonsPackagePtr jonsPkg);
+        Model* GetModel(const ModelID modelID);
 
-        MaterialPtr LoadMaterial(const std::string& assetName, const JonsPackagePtr jonsPkg);
-        MaterialPtr GetMaterial(const std::string& materialName);
+        MaterialID LoadMaterial(const std::string& assetName, const JonsPackagePtr jonsPkg);
+        Material* GetMaterial(const MaterialID materialID);
 
-        SkyboxPtr LoadSkybox(const std::string& skyboxName, const JonsPackagePtr jonsPkg);
-        SkyboxPtr GetSkybox(const std::string& skyboxName);
+        SkyboxID LoadSkybox(const std::string& skyboxName, const JonsPackagePtr jonsPkg);
+        Skybox* GetSkybox(const SkyboxID skyboxID);
 
         const IDMap<Mat4>& GetTransformStorage() const;
 
 
     private:
+        typedef IDMap<Model> ModelMap;
+        typedef IDMap<Material> MaterialMap;
+        typedef IDMap<Skybox> SkyboxMap;
+
 		IMemoryAllocatorPtr mMemoryAllocator;
-        DataPool<Mat4> mTransformCache;
         DX11Renderer& mRenderer;
 
-        std::vector<ModelPtr> mModels;
-        std::vector<MaterialPtr> mMaterials;
-        std::vector<SkyboxPtr> mSkyboxes;
+        ModelMap mModels;
+        MaterialMap mMaterials;
+        SkyboxMap mSkyboxes;
     };
 }
