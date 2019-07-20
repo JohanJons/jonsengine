d4a01487c3ee6f38ee4c5133806fa17d24a54e5f
JohanJons <jons.johan@gmail.com>
2019-07-20 16:58:35 +0200

    minor stuff

---
+++ JonsEngine/Scene/TerrainQuadTree.cpp
@@ -0,0 +1,6 @@
+#include "Scene/TerrainQuadTree.h"
+
+namespace JonsEngine
+{
+
+}
---
+++ JonsEngine/Scene/TerrainQuadTree.h
@@ -0,0 +1,12 @@
+#pragma once¨
+
+namespace JonsEngine
+{
+    class TerrainQuadTree
+    {
+    public:
+        TerrainQuadTree( const std::vector<uint8_t>& heightmapData, uint32_t patchMinSize, );
+
+    private:
+    }
+}
