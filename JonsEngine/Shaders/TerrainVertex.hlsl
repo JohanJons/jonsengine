18c8954872a8e2711032037219dc598f69f8048a
JohanJons <jons.johan@gmail.com>
2020-03-18 20:52:20 +0100

    Fix for no morph at last LOD level

--- JonsEngine/Shaders/TerrainVertex.hlsl
+++ JonsEngine/Shaders/TerrainVertex.hlsl
@@ -38,40 +38,52 @@ VertexOut vs_main(VertexIn input)
 	const float4x4 worldTransform = transpose( gTerrainWorldTransforms.Load( transformIndex ) );
 	const uint LODlevel = gTerrainLODLevels.Load( transformIndex );
 	const float4 worldPos = mul( worldTransform, float4( input.mPosition, 1 ) );
+	bool doMorphing = LODlevel > 0;
 
-	float4 preMorphPos = worldPos;
-	float2 preMorphTexcoord = GetTextureCoordinates( preMorphPos.xyz );
-	preMorphPos.y += SampleHeightmap( preMorphTexcoord ) * gHeightModifier;
+	float morphLerpK = 0.0f;
+	float2 texcoord;
+	float4 outWorldPos = worldPos;
+	if ( doMorphing )
+	{
+		float4 preMorphPos = worldPos;
+		float2 preMorphTexcoord = GetTextureCoordinates( preMorphPos.xyz );
+		preMorphPos.y += SampleHeightmap( preMorphTexcoord ) * gHeightModifier;
 
-	float scaleX = worldTransform[ 0 ][ 0 ];
-	float scaleZ = worldTransform[ 2 ][ 2 ];
+		float scaleX = worldTransform[ 0 ][ 0 ];
+		float scaleZ = worldTransform[ 2 ][ 2 ];
 
-	float cameraDistanceToVertex = distance( preMorphPos.xyz, gWorldEyePos );
-	float2 morphConstants = gLODMorphConstants.Load( LODlevel );
+		float cameraDistanceToVertex = distance( preMorphPos.xyz, gWorldEyePos );
+		float2 morphConstants = gLODMorphConstants.Load( LODlevel );
 
-	float morphLerpK  = 1.0f - clamp( morphConstants.x - cameraDistanceToVertex * morphConstants.y, 0.0f, 1.0f );
+		morphLerpK  = 1.0f - clamp( morphConstants.x - cameraDistanceToVertex * morphConstants.y, 0.0f, 1.0f );
 
-	float meshSize = ( float) gTerrainMeshSize;
-	float gridDimHalf = meshSize / 2.0f;
-	float oneOverGridDim = 2.0f / meshSize;
+		float meshSize = ( float) gTerrainMeshSize;
+		float gridDimHalf = meshSize / 2.0f;
+		float oneOverGridDim = 2.0f / meshSize;
 
-	float2 fracPart = ( frac( input.mPosition.xz * float2( gridDimHalf, gridDimHalf ) ) * float2( oneOverGridDim, oneOverGridDim ) ) * float2( scaleX, scaleZ );
-	preMorphPos.xz = preMorphPos.xz - ( fracPart * morphLerpK );
+		float2 fracPart = ( frac( input.mPosition.xz * float2( gridDimHalf, gridDimHalf ) ) * float2( oneOverGridDim, oneOverGridDim ) ) * float2( scaleX, scaleZ );
+		preMorphPos.xz = preMorphPos.xz - ( fracPart * morphLerpK );
 
-	float2 postMorphTexcoord = GetTextureCoordinates( preMorphPos.xyz );
-
-	float4 postMorphPos = worldPos;
-	postMorphPos.xz = preMorphPos.xz;
-	postMorphPos.y += SampleHeightmap( postMorphTexcoord ) * gHeightModifier;
+		float2 postMorphTexcoord = GetTextureCoordinates( preMorphPos.xyz );
+		
+		texcoord = postMorphTexcoord;
+		outWorldPos.xz = preMorphPos.xz;
+		outWorldPos.y += SampleHeightmap( postMorphTexcoord ) * gHeightModifier;
+	}
+	else
+	{
+		texcoord = GetTextureCoordinates( outWorldPos.xyz );
+		outWorldPos.y += SampleHeightmap( texcoord ) * gHeightModifier;
+	}
 
 	const int2 offset = 0;
 	const int mipmap = 0;
-	float3 normal = gNormalMap.SampleLevel( gLinearSampler, postMorphTexcoord, mipmap, offset ).rgb;
+	float3 normal = gNormalMap.SampleLevel( gLinearSampler, texcoord, mipmap, offset ).rgb;
 	normal *= 2.0;
 	normal -= 1.0;
 
 	VertexOut ret;
-	ret.mPosition = mul( gFrameViewProj, postMorphPos );
+	ret.mPosition = mul( gFrameViewProj, outWorldPos );
 #ifdef TERRAIN_DEBUG_NORMAL
 	ret.mWorldNormal = normal;
 #endif
